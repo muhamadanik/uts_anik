824355ccb7d3cb30e15f1505c5e2e5de0240eea8
muhamadanik <anikbusinessman@gmail.com>
2019-11-12 15:33:40 +0700

    uts_anik

---
+++ application/views/template/sidebar.php
@@ -0,0 +1,23 @@
+<aside class="main-sidebar">
+<section class="sidebar">
+    <div class="user-panel">
+      <div class="pull-left image">
+        <img src="<?= base_url() ?>assets/dist/img/anik.jpg" class="img-circle" alt="User Image">
+      </div>
+      <div class="pull-left info">
+        <p>Muhamad Anik</p>
+        <a href="#"><i class="fa fa-circle text-success"></i>Maintenence</a>
+      </div>
+    </div>
+
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">DATA MAHASISWA</li>
+        <li class="active"><a href="c_crud/tampil"><i class="fa fa-link"></i>Dashboard</a></li>
+        <!-- <li class="active"><a href="crud/tambah"><i class="fa fa-link"></i>Tambah Data</a></li> -->
+        <li class="active"><a href="c_crud/tampil2"><i class="fa fa-link"></i>Riwayat Log</a></li>
+
+      </ul>
+      <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+  </aside>
